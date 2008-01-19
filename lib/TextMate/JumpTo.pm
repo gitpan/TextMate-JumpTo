fa74abf210cd9b2aa5498c03a010be863eb1a226
Andy Armstrong <andy@hexten.net>
2008-01-19 14:16:09 +0000

    Import of ANDYA/TextMate-JumpTo-0.01 from CPAN.
    gitpan-cpan-distribution: TextMate-JumpTo
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         ANDYA/TextMate-JumpTo-0.01.tar.gz
    gitpan-cpan-author:       ANDYA
    gitpan-cpan-maturity:     released

---
+++ lib/TextMate/JumpTo.pm
@@ -0,0 +1,121 @@
+package TextMate::JumpTo;
+
+use warnings;
+use strict;
+use HTML::Tiny;
+use File::Spec;
+use Carp;
+
+use base qw(Exporter);
+
+our @EXPORT_OK = qw(jumpto);
+
+=head1 NAME
+
+TextMate::JumpTo - Tell TextMate to jump to a particular file, line
+
+=head1 VERSION
+
+This document describes TextMate::JumpTo version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+    use TextMate::JumpTo qw(jumpto);
+    
+    jumpto( file => 'mysrc.pl', line => 123 );
+  
+=head1 DESCRIPTION
+
+=head1 INTERFACE 
+
+=head2 C<< jumpto >>
+
+Instruct TextMate to jump to the specified file, line and column. The
+arguments are a list of key, value pairs:
+
+    jumpto( file => 'splendid.pl', line => 12, column => 3 );
+
+Possible arguments are:
+
+=over
+
+=item * C<file>
+
+The path to the file to go to.
+
+=item * C<line>
+
+The (one based) line number to go to.
+
+=item * C<column>
+
+The (one based) column to go to.
+
+=item * C<bg>
+
+True to leave TextMate in the background. By default a call to C<jumpto>
+will bring TextMate to the foreground.
+
+=back
+
+=cut
+
+sub jumpto {
+    croak
+      "Odd number of args to jumpto, needs a list of key => value pairs"
+      if @_ % 2;
+    my %args = @_;
+    my $bg   = delete $args{bg};
+    croak "You must supply one or more of file, line, column"
+      unless grep defined $args{$_}, qw(file line column);
+    if ( my $file = delete $args{file} ) {
+        $args{url} = "file://" . File::Spec->rel2abs( $file );
+    }
+    _open( 'txmt://open?' . HTML::Tiny->new->query_encode( \%args ),
+        $bg );
+}
+
+# Open a URL on Mac OS.
+sub _open {
+    my ( $url, $bg ) = @_;
+    my @cmd = ( '/usr/bin/open', ( $bg ? ( '-g' ) : () ), $url );
+    system @cmd and croak "Can't open $url ($?)";
+}
+
+1;
+__END__
+
+=head1 CONFIGURATION AND ENVIRONMENT
+  
+TextMate::JumpTo requires no configuration files or environment variables.
+
+=head1 DEPENDENCIES
+
+None.
+
+=head1 INCOMPATIBILITIES
+
+None reported.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests to
+C<bug-textmate-jumpto@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 AUTHOR
+
+Andy Armstrong  C<< <andy@hexten.net> >>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2008, Andy Armstrong C<< <andy@hexten.net> >>.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
